90dd7b2730fb55bc94561f120c1f02f5fc57a257
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2026-04-11 14:59:14 +1000

    send empty dialog when closing to force close search screen on bedrock

--- chestshop-database-bukkit/src/main/java/io/github/md5sha256/chestshopdatabase/util/DialogUtil.java
+++ chestshop-database-bukkit/src/main/java/io/github/md5sha256/chestshopdatabase/util/DialogUtil.java
@@ -2,8 +2,11 @@ package io.github.md5sha256.chestshopdatabase.util;
 
 import io.papermc.paper.dialog.Dialog;
 import io.papermc.paper.dialog.DialogResponseView;
+import io.papermc.paper.registry.data.dialog.DialogBase;
 import io.papermc.paper.registry.data.dialog.action.DialogAction;
+import io.papermc.paper.registry.data.dialog.type.DialogType;
 import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickCallback;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,6 +19,14 @@ public class DialogUtil {
 
     public static final DialogAction CLOSE_DIALOG_ACTION =
             DialogAction.customClick(DialogUtil::closeDialog, DEFAULT_CALLBACK_OPTIONS);
+
+    public static final DialogBase EMPTY_DIALOG_BASE = DialogBase
+            .builder(Component.empty())
+            .build();
+
+    public static final Dialog EMPTY_DIALOG = Dialog.create(reg -> reg.empty()
+            .base(EMPTY_DIALOG_BASE)
+            .type(DialogType.notice()));
 
     public static void closeDialog(@NotNull DialogResponseView view, @NotNull Audience audience) {
         audience.closeDialog();
